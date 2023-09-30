2e5d096468214e40c35a3eab1ecadf4578fb1af1
Ruan Bertin <ruanbertin@hotmail.com>
2023-09-30 01:31:42 -0300

    Feat: Adicionando mascara ao input

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,31 +1,51 @@
-import { useState } from "react"
-import style from "./Formulario.module.css"
+import React from 'react';
+import InputMask from 'react-input-mask';
+import { useState } from 'react';
+import style from './Formulario.module.css';
 
-const Formulario = ({onCalculaImc}) =>{
-  const [altura,setAltura] = useState(0);
+const Formulario = ({ onCalculaImc }) => {
+  const [altura, setAltura] = useState(0);
   const [peso, setPeso] = useState(0);
+
   const calculaImc = () => {
-    const imc = peso / (altura * altura)
+    const imc = peso / (altura * altura);
     onCalculaImc(imc);
-    console.log(imc.toFixed(2))
-  }
+  };
 
-  return(
+  return (
     <>
-    <form className={style.formulario}>
-      <div className={style.campoAltura}>
-        <label className={style.label}>Digite sua altura<span className={style.span}>(ex: 1,75)</span></label>
-        <input type="number" onBlur={(e) => setAltura(Number(e.target.value))} placeholder="Metros"/>
-        <button type="button" className={style.btnCalcular} onClick={calculaImc}>Calcular</button>
-      </div>
+      <form className={style.formulario}>
+        <div className={style.campoAltura}>
+          <label className={style.label}>
+            Digite sua altura
+            <span className={style.span}>(ex: 1,75)</span>
+          </label>
+          <InputMask
+            mask="9,99"
+            onBlur={(e) => setAltura(Number(e.target.value.replace(',', '.')))}
+            placeholder="Metros"
+          >
+            {(inputProps) => <input {...inputProps} />}
+          </InputMask>
+          <button type="button" className={style.btnCalcular} onClick={calculaImc}>
+            Calcular
+          </button>
+        </div>
 
-      <div className={style.campoPeso}>
-        <label className={style.label}>Digite seu peso <span className={style.span}>(ex: 80kg)</span></label>
-        <input type="number" onBlur={(e) => setPeso(Number(e.target.value))} placeholder="quilos"/>
-      </div>
-    </form>
+        <div className={style.campoPeso}>
+          <label className={style.label}>
+            Digite seu peso
+            <span className={style.span}>(ex: 80kg)</span>
+          </label>
+          <input
+            type="number"
+            onBlur={(e) => setPeso(Number(e.target.value))}
+            placeholder="quilos"
+          />
+        </div>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
